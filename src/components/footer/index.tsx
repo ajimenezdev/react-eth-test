06fd623a1c4e8b833804ef596908a63b9cff079d
ajimenezdev <ajmjimens@gmail.com>
2019-09-10 15:33:56 +0200

    Update footer

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -1,9 +1,27 @@
 import React from "react";
+import { Icon, IconButton } from "@material-ui/core";
+import "./footer.css";
 
 const Footer: React.FC = () => {
   return (
     <footer className="App-footer">
-      <p>Footer</p>
+      <p className="footer-author">
+        created by:
+        <a href="https://alvarojimenezmartin.com" target="_blank">
+          Alvaro Jimenez Martin
+        </a>
+      </p>
+      <IconButton
+        aria-label="open"
+        onClick={() =>
+          window.open("https://github.com/ajimenezdev/react-eth-test", "_blank")
+        }
+      >
+        <div className="footer-sourceContainer">
+          <Icon className="footer-sourceIcon">code</Icon>
+          <span className="footer-sourceText">Source Code</span>
+        </div>
+      </IconButton>
     </footer>
   );
 };
